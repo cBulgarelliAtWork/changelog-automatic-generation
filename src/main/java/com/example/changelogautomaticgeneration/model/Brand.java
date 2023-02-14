654a63249fdeffc3d60432199c3509f62368674e
Cristian Bulgarelli <cristian.bulgarelli@overit.it>
2023-02-14 17:45:52 +0100

    add: GC-013 brand model class

---
+++ src/main/java/com/example/changelogautomaticgeneration/model/Brand.java
@@ -0,0 +1,25 @@
+package com.example.changelogautomaticgeneration.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "brand")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@ToString
+public class Brand implements Serializable {
+    @Id
+    @GeneratedValue
+    @EqualsAndHashCode.Include
+    private Long id;
+    @Column(name = "description", nullable = false, length = 128)
+    @NotNull
+    private String description;
+}
